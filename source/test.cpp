2c36a36c42400c402dceedd87957050faeeb052a
underflow101 <ikarus125@gmail.com>
2020-11-09 23:55:31 +0900

    feat: Test for format-checker
    Signed-off-by: underflow101 <ikarus125@gmail.com>

---
+++ source/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void main() 
+
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  int i = 0;
+  cout << "i: " << i << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" << endl;
+  return;}
